6aabe4c87b71d75c1cf61d3b016352470562482b
Theodore Koterwas <theodore.koterwas@oucs.ox.ac.uk>
2015-06-14 17:50:14 +0100

    create window event

--- www/microphone_levels_plugin.js
+++ www/microphone_levels_plugin.js
@@ -1,25 +1,30 @@
 cordova = require('cordova');
 exec = require('cordova/exec');
+channels = require('cordova/channels');
 
 
 function MicrophoneLevels() {
     this.cordova = null;
-}
+    this.channels = {
+      	microphonelevels:cordova.addWindowEventHandler("microphonelevels")
+    };
+};
 
 MicrophoneLevels.prototype.start = function() {
-    exec(null, onError, "MicrophoneLevels", "start", null);
+    exec(null, onError, "MicrophoneLevels", "start", []]);
 };
 
 MicrophoneLevels.prototype.stop = function() {
-    exec(null, onError, "MicrophoneLevels", "stop", null);
+    exec(null, onError, "MicrophoneLevels", "stop", []]);
 };
 
 MicrophoneLevels.prototype.levels = function() {
-    exec(onLevels, onError, "MicrophoneLevels", "levels", null);
+    exec(onLevels, onError, "MicrophoneLevels", "levels", []]);
 };
 
 var onLevels = function(params) {
     console.log(params);
+    cordova.fireWindowEvent("microphonelevels", info);
 }
 
 var onError = function(err) {
@@ -27,4 +32,6 @@ var onError = function(err) {
     console.log(err);
 };
 
-module.exports = new MicrophoneLevels();
+var microphoneLevels = new MicrophoneLevels();
+
+module.exports = microphoneLevels;
