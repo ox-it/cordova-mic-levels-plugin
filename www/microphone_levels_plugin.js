5ae3721b49a8944b5603556f472e24726af3c729
Andrew Haith <andyhaith@gmail.com>
2015-06-18 14:48:44 +0100

    Fix some errors.

--- www/microphone_levels_plugin.js
+++ www/microphone_levels_plugin.js
@@ -1,6 +1,6 @@
 cordova = require('cordova');
 exec = require('cordova/exec');
-channels = require('cordova/channels');
+channel = require('cordova/channel');
 
 
 function MicrophoneLevels() {
@@ -12,7 +12,8 @@ function MicrophoneLevels() {
         this.channels[key].onHasSubscribersChange = MicrophoneLevels.onHasSubscribersChange;
     }
     this.timer = null;
-};
+}
+
 /**
  * Event handlers for when callbacks get registered.
  * Keep track of how many handlers we have so we can start and stop the native microphone listener
@@ -22,38 +23,36 @@ MicrophoneLevels.onHasSubscribersChange = function() {
   	// If we just registered the first handler, make sure native listener is started.
   	if (this.numHandlers === 1 && handlers() === 1) {
       	exec(microphoneLevels.onLevels, microphonelevels.onError, "MicrophoneLevels", "start", []);
-  	} 
+  	}
   	else if (handlers() === 0) {
       	exec(null, null, "MicrophoneLevels", "stop", []);
   	}
 };
 
 MicrophoneLevels.prototype.start = function() {
-    exec(null, onError, "MicrophoneLevels", "start", []]);
+    exec(null, onError, "MicrophoneLevels", "start", []);
     //startTimer
     this.timer = window.setInterval(this.levels, 0.03);
 };
 
 MicrophoneLevels.prototype.stop = function() {
-    exec(null, onError, "MicrophoneLevels", "stop", []]);
+    exec(null, onError, "MicrophoneLevels", "stop", []);
     //stopTimer
     window.clearInterval(this.timer);
 };
 
 MicrophoneLevels.prototype.levels = function() {
-    exec(onLevels, onError, "MicrophoneLevels", "levels", []]);
+    exec(onLevels, onError, "MicrophoneLevels", "levels", []);
 };
 
 var onLevels = function(params) {
     console.log(params);
     cordova.fireWindowEvent("microphonelevels", params);
-}
+};
 
 var onError = function(err) {
     console.log("Error");
     console.log(err);
 };
 
-var microphoneLevels = new MicrophoneLevels();
-
-module.exports = microphoneLevels;
+module.exports = new MicrophoneLevels();
